e217de8a98c9b9b947b438e84c26f37c458242bc
Yevgen Safronov <sejoker@gmail.com>
2015-11-15 16:52:22 +0100

    add intro

---
+++ index.js
@@ -0,0 +1,25 @@
+#!/usr/bin/env node
+
+var adventure = require('adventure')
+var shop = adventure({
+  name: 'learnrx',
+  bg: 'white',
+  fg: 'magenta',
+  title: 'Functional Programming in JavaScript'
+})
+
+var problems = fs.readdirSync(path.resolve(__dirname, 'problems'))
+problems.filter(function (problem) {
+  return problem.match(/^[^.].*\.js$/)
+}).forEach(function (problem) {
+  var name = problem.replace(/\.js$/, '').split('-').map(function (p) {
+    if (p === 'npm')
+      return p
+    return p.charAt(0).toUpperCase() + p.slice(1)
+  }).join(' ')
+  shop.add(name, function () {
+    return require('./problems/' + problem)
+  })
+})
+
+shop.execute(process.argv.slice(2))
---
+++ problems/00-intro.js
@@ -0,0 +1,33 @@
+exports.problem = function () { /*
+This is a series of interactive exercises for learning Microsoft's Reactive Extensions (Rx) Library for JavaScript. 
+So why is the title "Functional Programming in JavaScript"? Well it turns out that the key to learning Rx is 
+training yourself to use functional programming to manipulate collections.  Functional programming provides 
+developers with the tools to abstract common collection operations into reusable, composable building blocks. 
+You'll be surprised to learn that most of the operations you perform on collections can be accomplished with
+**five simple functions** (some native to JavaScript and some included in the [RxJS library](https://github.com/ReactiveX/RxJS)):
+
+1. map
+2. filter
+3. concatAll
+4. reduce
+5. zip
+
+Here's my promise to you: if you learn these 5 functions your code will become shorter, more self-descriptive,
+and more durable. Also, for reasons that might not be obvious right now, you'll learn that these five functions
+hold the key to simplifying asynchronous programming. Once you've finished this tutorial you'll also have all
+the tools you need to easily avoid race conditions, propagate and handle asynchronous errors, and sequence
+events and AJAX requests. In short,
+**these 5 functions will probably be the most powerful, flexible, and useful functions you'll ever learn.**
+
+To process to the next exercise run
+`learnrx verify next`
+*/}.toString().split('\n').slice(1,-1).join('\n')
+
+exports.verify = function (args,cb) {
+  if (args.join('').toLowerCase() !== 'next') {
+    console.log('run `learnrx verify next` to proceed');
+    return cb(false)
+  }
+
+  return cb(true)
+}
