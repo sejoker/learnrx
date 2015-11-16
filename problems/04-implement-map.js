d23dcbdcbe8c6e14e83776737e4f84ccd33f367b
Yevgen Safronov <sejoker@gmail.com>
2015-11-16 23:42:36 +0100

    fix message

--- problems/04-implement-map.js
+++ problems/04-implement-map.js
@@ -47,6 +47,7 @@ Array.prototype.forEach = function () {
 }
 
 exports.verify = verify({ modeReset: true}, function (args, t) {
+  useForEach = false
   t.equal(args.length, 1, 'learnrx verify YOURFILE.js')
 
   require(path.resolve(args[0]))
@@ -54,6 +55,7 @@ exports.verify = verify({ modeReset: true}, function (args, t) {
   var result = arr.map(function (x) { return x + 1 })
   t.equal(JSON.stringify(arr), '[1,2,3]', 'Original array stays unchanged')
   t.equal(JSON.stringify(result), '[2,3,4]', 'Expected that [1,2,3].map(function(x) { return x + 1}) would equal [2,3,4].')
-  t.ok(useForEach, 'You did not use Array.forEach')
+  t.ok(useForEach, 'You did use Array.forEach')
+  t.end()
 })
 
