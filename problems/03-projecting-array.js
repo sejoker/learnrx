fa3cafe5298b1c80438c8667bac9b8b5bdea2179
Yevgen Safronov <sejoker@gmail.com>
2015-11-17 23:17:09 +0100

    problem 3 rewrite, use of deepEqual, videos as input params

--- problems/03-projecting-array.js
+++ problems/03-projecting-array.js
@@ -9,8 +9,50 @@ Exercise 3: Project an array of videos into an array of {id,title} pairs using f
 
 For each video, add a projected {id, title} pair to the videoAndTitlePairs array.
 
-module.exports = function() {
-  var newReleases = [
+Input example:
+[
+  {
+    "id": 70111470,
+    "title": "Die Hard",
+    "boxart": "http://cdn-0.nflximg.com/images/2891/DieHard.jpg",
+    "uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+    "rating": [4.0],
+    "bookmark": []
+  }
+]
+
+module.exports = function(videos) {
+
+  var videoAndTitlePairs = [];
+
+  // ------------ INSERT CODE HERE! -----------------------------------
+  // Use forEach function to accumulate {id, title} pairs from each video.
+  // Put the results into the videoAndTitlePairs array using the Array's
+  // push() method. Example: videoAndTitlePairs.push(newItem);
+  // ------------ INSERT CODE HERE! -----------------------------------
+
+
+  return videoAndTitlePairs;
+}  
+*/}.toString().split('\n').slice(1,-1).join('\n')
+
+exports.solution = function (videos) { /*
+module.exports = function(videos) {
+  
+  var videoAndTitlePairs = [];
+
+  videos.forEach(function(video) {
+    videoAndTitlePairs.push({id:video.id, title: video.title});
+  });
+
+  return videoAndTitlePairs;
+}
+*/}.toString().split('\n').slice(1,-1).join('\n')
+
+var verify = require('adventure-verify')
+var path = require('path')
+
+var newReleases = [
     {
       "id": 70111470,
       "title": "Die Hard",
@@ -43,69 +85,7 @@ module.exports = function() {
       "rating": [5.0],
       "bookmark": [{ id:432534, time:65876586 }]
     }
-  ],
-  videoAndTitlePairs = [];
-
-  // ------------ INSERT CODE HERE! -----------------------------------
-  // Use forEach function to accumulate {id, title} pairs from each video.
-  // Put the results into the videoAndTitlePairs array using the Array's
-  // push() method. Example: videoAndTitlePairs.push(newItem);
-  // ------------ INSERT CODE HERE! -----------------------------------
-
-
-  return videoAndTitlePairs;
-}  
-*/}.toString().split('\n').slice(1,-1).join('\n')
-
-exports.solution = function () { /*
-module.exports = function() {
-  var newReleases = [
-      {
-        "id": 70111470,
-        "title": "Die Hard",
-        "boxart": "http://cdn-0.nflximg.com/images/2891/DieHard.jpg",
-        "uri": "http://api.netflix.com/catalog/titles/movies/70111470",
-        "rating": [4.0],
-        "bookmark": []
-      },
-      {
-        "id": 654356453,
-        "title": "Bad Boys",
-        "boxart": "http://cdn-0.nflximg.com/images/2891/BadBoys.jpg",
-        "uri": "http://api.netflix.com/catalog/titles/movies/70111470",
-        "rating": [5.0],
-        "bookmark": [{ id:432534, time:65876586 }]
-      },
-      {
-        "id": 65432445,
-        "title": "The Chamber",
-        "boxart": "http://cdn-0.nflximg.com/images/2891/TheChamber.jpg",
-        "uri": "http://api.netflix.com/catalog/titles/movies/70111470",
-        "rating": [4.0],
-        "bookmark": []
-      },
-      {
-        "id": 675465,
-        "title": "Fracture",
-        "boxart": "http://cdn-0.nflximg.com/images/2891/Fracture.jpg",
-        "uri": "http://api.netflix.com/catalog/titles/movies/70111470",
-        "rating": [5.0],
-        "bookmark": [{ id:432534, time:65876586 }]
-      }
-    ],
-    videoAndTitlePairs = [];
-
-  newReleases.forEach(function(video) {
-    videoAndTitlePairs.push({id:video.id, title: video.title});
-  });
-
-  return videoAndTitlePairs;
-}
-*/}.toString().split('\n').slice(1,-1).join('\n')
-
-var verify = require('adventure-verify')
-var path = require('path')
-require('../src/utils')
+  ]
 
 exports.verify = verify({ modeReset: false }, function (args,t) {
   t.equal(args.length, 1, 'learnrx verify YOURFILE.js')
@@ -113,10 +93,8 @@ exports.verify = verify({ modeReset: false }, function (args,t) {
   var f = require(path.resolve(args[0]))
   t.equal(typeof f, 'function', 'you exported a function')
 
-  var expected = '[{\"id\":675465,\"title\":\"Fracture\"},{\"id\":65432445,\"title\":\"The Chamber\"},{\"id\":70111470,\"title\":\"Die Hard\"},{\"id\":654356453,\"title\":\"Bad Boys\"}]';
-  var videoAndTitlePairs = f()
+  var expected = newReleases.map(function (video) { return { id:video.id, title: video.title } })
 
-  videoAndTitlePairs = videoAndTitlePairs.sortBy(function(video) { return video.id });
-  t.equal(JSON.stringify(videoAndTitlePairs), expected)
+  t.deepEqual(f(newReleases), expected)
   t.end()
 })
